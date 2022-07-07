ade60a6c639aa4a8c3a4e5d0c32cf3508f520154
maxim.irmscher <maxim.irmscher@idealo.de>
2022-07-07 15:56:33 +0200

    implement redis cache

--- src/main/java/com/example/Warehouse/repository/DefaultProductRepositoryImpl.java
+++ src/main/java/com/example/Warehouse/repository/DefaultProductRepositoryImpl.java
@@ -3,19 +3,24 @@ package com.example.Warehouse.repository;
 import com.example.Warehouse.entity.DefaultProduct;
 import com.example.Warehouse.repository.jpa.DefaultProductEntityJpaRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class DefaultProductRepositoryImpl implements DefaultProductRepository {
 
     private final DefaultProductEntityJpaRepository defaultProductEntityJpaRepository;
 
+    @Cacheable(value = "userProductWarehouseCache")
     @Override
     public List<DefaultProduct> findAll() {
+        log.info("get default products from repo without cache");
         return defaultProductEntityJpaRepository.findAll().stream()
                 .map(DefaultProduct::from)
                 .collect(Collectors.toList());
--- src/main/java/com/example/Warehouse/repository/ProductComponentRepositoryImpl.java
+++ src/main/java/com/example/Warehouse/repository/ProductComponentRepositoryImpl.java
@@ -3,19 +3,24 @@ package com.example.Warehouse.repository;
 import com.example.Warehouse.entity.ProductComponent;
 import com.example.Warehouse.repository.jpa.ProductComponentEntityJpaRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class ProductComponentRepositoryImpl implements ProductComponentRepository {
 
     private final ProductComponentEntityJpaRepository productComponentEntityJpaRepository;
 
+    @Cacheable(value="productComponentWarehouseCache")
     @Override
     public List<ProductComponent> findAll() {
+        log.info("get product components from repo without cache");
         return productComponentEntityJpaRepository.findAll().stream()
                 .map(ProductComponent::from)
                 .collect(Collectors.toList());
