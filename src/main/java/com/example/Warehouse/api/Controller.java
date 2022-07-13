74c2bff55ec7f09722e3c04ee007c613761e49ca
maxim.irmscher <maxim.irmscher@idealo.de>
2022-07-13 16:57:24 +0200

    add ITtest

--- src/main/java/com/example/Warehouse/api/Controller.java
+++ src/main/java/com/example/Warehouse/api/Controller.java
@@ -4,11 +4,15 @@ import com.example.Warehouse.api.dto.DefaultProductResponse;
 import com.example.Warehouse.api.dto.ProductComponentResponse;
 import com.example.Warehouse.domain.WarehouseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,6 +21,7 @@ public class Controller {
     private final WarehouseService warehouseService;
 
     @GetMapping("/defaultProducts")
+    @ResponseStatus(OK)
     public List<DefaultProductResponse> getAllDefaultProducts() {
         return warehouseService.getAllDefaultProducts()
                 .stream()
@@ -25,6 +30,7 @@ public class Controller {
     }
 
     @GetMapping("/components")
+    @ResponseStatus(OK)
     public List<ProductComponentResponse> getAllProductComponents() {
 
         return warehouseService
---
+++ src/test/java/com/example/Warehouse/api/ControllerTestIT.java
@@ -0,0 +1,51 @@
+package com.example.Warehouse.api;
+
+
+import com.example.Warehouse.domain.WarehouseService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(Controller.class)
+public class ControllerTestIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private WarehouseService warehouseService;
+
+    @Test
+    void get_all_default_products() {
+        try {
+            mockMvc.perform(get("/defaultProducts"))
+                    .andExpect(status().isOk());
+
+            verify(warehouseService).getAllDefaultProducts();
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    void get_all_product_components() {
+        try {
+            mockMvc.perform(get("/components"))
+                    .andExpect(status().isOk());
+
+            verify(warehouseService).getAllProductComponents();
+        } catch (Exception e) {
+            fail();
+        }
+    }
+}
